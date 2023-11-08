e6dacd2a18b3e914dee400eeb470f3febba1f896
guanxiaoyin <shen930909@mail.ustc.edu.cn>
2023-11-08 09:58:30 +0800

    feat: add jsx dev

--- packages/react/src/jsx.ts
+++ packages/react/src/jsx.ts
@@ -13,7 +13,7 @@ const ReactElement = function (
   type: Type,
   key: Key,
   ref: Ref,
-  props: Props
+  props: Props,
 ): ReactElementType {
   const element = {
     $$typeof: REACT_ELEMENT_TYPE,
@@ -60,4 +60,28 @@ export const jsx = (type: ElementType, config: any, ...maybeChildren: any) => {
   return ReactElement(type, key, ref, props);
 };
 
-export const jsxDEV = jsx;
+export const jsxDEV = (type: ElementType, config: any) => {
+  let key: Key = null;
+  const props: Props = {};
+  let ref: Ref = null;
+  for (const prop in config) {
+    const val = config[prop];
+    if (prop === "key") {
+      if (val !== undefined) {
+        key = "" + val;
+      }
+      continue;
+    }
+    if (prop === "ref") {
+      if (val !== undefined) {
+        ref = val;
+      }
+      continue;
+    }
+    if ({}.hasOwnProperty.call(config, prop)) {
+      props[prop] = val;
+    }
+  }
+
+  return ReactElement(type, key, ref, props);
+};
